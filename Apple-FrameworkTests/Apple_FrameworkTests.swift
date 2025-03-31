aae60429498c44a48c94194472b79807b37dca91
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-04-01 00:45:55 +0530

    Create Apple_FrameworkTests.swift

---
+++ Apple-FrameworkTests/Apple_FrameworkTests.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworkTests.swift
+//  Apple-FrameworkTests
+//
+//  Created by Yash Raghuvanshi on 31/03/25.
+//
+
+import Testing
+@testable import Apple_Framework
+
+struct Apple_FrameworkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
