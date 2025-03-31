168a9f2acdaf33c6a95b6de2f9a8c8f8302b9465
Yash Raghuvanshi <yashraghuvans@gmail.com>
2025-03-31 20:05:19 +0530

    Create Apple_FrameworkApp.swift

---
+++ Apple-Framework/Apple_FrameworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworkApp.swift
+//  Apple-Framework
+//
+//  Created by Yash Raghuvanshi on 31/03/25.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
